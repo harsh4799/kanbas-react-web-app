5fcc435c6aa43a16b1e8bbd2cae75d6b2cf3475c
Harshit Pandey <helloharshit68@gmail.com>
2024-02-22 10:05:06 -0500

    Add assignment

--- src/Kanbas/Courses/Assignments/Editor/index.tsx
+++ src/Kanbas/Courses/Assignments/Editor/index.tsx
@@ -4,23 +4,24 @@ import { assignments } from "../../../Database";
 function AssignmentEditor() {
   const { assignmentId } = useParams();
   const assignment = assignments.find(
-    (assignment) => assignment._id === assignmentId);
+    (assignment) => assignment._id === assignmentId,
+  );
   const { courseId } = useParams();
   const navigate = useNavigate();
-  const handleSave = () => {
-    console.log("Actually saving assignment TBD in later assignments");
+  const handleSaving = () => {
     navigate(`/Kanbas/Courses/${courseId}/Assignments`);
   };
   return (
     <div>
       <h2>Assignment Name</h2>
-      <input value={assignment?.title}
-             className="form-control mb-2" />
-      <button onClick={handleSave} className="btn btn-success ms-2 float-end">
+      <input value={assignment?.title} className="form-control mb-2" />
+      <button onClick={handleSaving} className="btn btn-success ms-2 float-end">
         Save
       </button>
-      <Link to={`/Kanbas/Courses/${courseId}/Assignments`}
-            className="btn btn-danger float-end">
+      <Link
+        to={`/Kanbas/Courses/${courseId}/Assignments`}
+        className="btn btn-danger float-end"
+      >
         Cancel
       </Link>
     </div>
--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -1,35 +1,88 @@
-import React from "react";
-import { FaCheckCircle, FaEllipsisV, FaPlusCircle } from "react-icons/fa";
+import React, { useState } from "react";
+import {
+  FaCheckCircle,
+  FaEllipsisV,
+  FaPlusCircle,
+  FaPlus,
+  FaChevronDown,
+  FaBook,
+} from "react-icons/fa";
 import { Link, useParams } from "react-router-dom";
 import { assignments } from "../../Database";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
 function Assignments() {
   const { courseId } = useParams();
   const assignmentList = assignments.filter(
-    (assignment) => assignment.course === courseId);
+    (assignment) => assignment.course === courseId,
+  );
+
+  const [value, setValue] = useState("");
+
+  const handleChanges = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
   return (
     <>
-      <ul className="list-group wd-modules">
+      <div className="row top-bar">
+        <div className="col-4">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search for Assignment"
+            onChange={handleChanges}
+          />
+        </div>
+        <div className="col top-button-bar d-flex justify-content-end">
+          <a> Group </a>
+
+          <a className="red">
+            <FaPlus /> Assignment{" "}
+          </a>
+
+          <a>
+            {" "}
+            <FaEllipsisV className="me-2 custom" />{" "}
+          </a>
+        </div>
+      </div>
+
+      <hr />
+
+      <ul className="list-group wd-assignments">
         <li className="list-group-item">
           <div>
             <FaEllipsisV className="me-2" /> ASSIGNMENTS
             <span className="float-end">
+              <span className="percent-box">40% of total</span>
               <FaCheckCircle className="text-success" />
-              <FaPlusCircle className="ms-2" /><FaEllipsisV className="ms-2" />
+              <FaPlusCircle className="ms-2" />
+              <FaEllipsisV className="ms-2" />
             </span>
           </div>
           <ul className="list-group">
             {assignmentList.map((assignment) => (
               <li className="list-group-item">
                 <FaEllipsisV className="me-2" />
+                <FaBook className="me-2" />
                 <Link
-                   to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}>{assignment.title}</Link>
+                  to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}
+                >
+                  {assignment.title}
+                </Link>
                 <span className="float-end">
-                  <FaCheckCircle className="text-success" /><FaEllipsisV className="ms-2" /></span>
-              </li>))}
+                  <FaCheckCircle className="text-success" />
+                  <FaEllipsisV className="ms-2" />
+                </span>
+              </li>
+            ))}
           </ul>
         </li>
       </ul>
     </>
-);}
-export default Assignments;
+  );
+}
 
+export default Assignments;
