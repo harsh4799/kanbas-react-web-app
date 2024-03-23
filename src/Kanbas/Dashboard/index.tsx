a72fcd198102a0a23729a9ca812787af78af67c9
Harshit Pandey <helloharshit68@gmail.com>
2024-03-23 10:46:08 -0400

    add dashboard

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,25 +1,69 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { courses } from "../Database";
-function Dashboard() {
+function Dashboard({
+  courses,
+  course,
+  setCourse,
+  addNewCourse,
+  deleteCourse,
+  updateCourse,
+}: {
+  courses: any[];
+  course: any;
+  setCourse: (course: any) => void;
+  addNewCourse: () => void;
+  deleteCourse: (course: any) => void;
+  updateCourse: () => void;
+}) {
   return (
     <div className="p-4">
-      <h1>Dashboard</h1>              <hr />
+      <h1>Dashboard</h1> <hr />
       <h2>Published Courses (6)</h2> <hr />
+      <input value={course.name} className="form-control"
+             onChange={(e) => setCourse({ ...course, name: e.target.value }) } />
+      <input value={course.number} className="form-control"
+             onChange={(e) => setCourse({ ...course, number: e.target.value }) } />
+      <input value={course.startDate} className="form-control" type="date"
+             onChange={(e) => setCourse({ ...course, startDate: e.target.value }) }/>
+      <input value={course.endDate} className="form-control" type="date"
+             onChange={(e) => setCourse({ ...course, endDate: e.target.value }) } />
+
+      <button onClick={addNewCourse} >
+        Add
+      </button>
+      <button onClick={updateCourse} >
+        Update
+      </button>
       <div className="row">
         <div className="row row-cols-1 row-cols-md-5 g-4">
           {courses.map((course) => (
             <div key={course._id} className="col" style={{ width: 300 }}>
               <div className="card">
-                <img src={`/images/${course.image}`} className="card-img-top"
-                     style={{ height: 180 }}/>
+                <img
+                  src={`/images/${course.image}`}
+                  className="card-img-top"
+                  style={{ height: 180 }}
+                />
                 <div className="card-body">
-                  <Link className="card-title" to={`/Kanbas/Courses/${course._id}`}
-                    style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                    {course.name} </Link>
+                  <Link
+                    className="card-title"
+                    to={`/Kanbas/Courses/${course._id}`}
+                    style={{
+                      textDecoration: "none",
+                      color: "navy",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {course.name}{" "}
+                  </Link>
                   <p className="card-text">{course.name}</p>
-                  <Link to={`/Kanbas/Courses/${course._id}`} className="btn btn-primary">
-                    Go </Link>
+                  <Link
+                    to={`/Kanbas/Courses/${course._id}`}
+                    className="btn btn-primary"
+                  >
+                    Go{" "}
+                  </Link>
                 </div>
               </div>
             </div>
--- src/Kanbas/Database/index.tsx
+++ src/Kanbas/Database/index.tsx
@@ -1,8 +1,8 @@
 import courses from "./courses.json";
-import assignments from './assignments.json';
-import enrollments from './enrollments.json';
-import grades from './grades.json';
-import users from './users.json'
-import modules from './modules.json'
-
-export {  courses , assignments, enrollments, grades, users, modules };
+import assignments from "./assignments.json";
+import enrollments from "./enrollments.json";
+import grades from "./grades.json";
+import users from "./users.json";
+import modules from "./modules.json";
+import db from "./db.json";
+export { db, courses, assignments, enrollments, grades, users, modules };
